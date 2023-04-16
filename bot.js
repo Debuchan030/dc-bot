149bc79420aeca2c47a5402233160a3cd6756a9d
Debuchan030 <debuchan030@gmail.com>
2023-04-16 21:55:59 +0800

    add some shit

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ const client = new Discord.Client();
 // 連上線時的事件
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    game = Discord.Game('努力學習跟各位大大說安安中');
-    client.change_presence(status=Discord.Status.idle, activity=game);
+    // game = Discord.Game('努力學習跟各位大大說安安中');
+    // client.change_presence(status=Discord.Status.idle, activity=game);
 });
 
 // 當 Bot 接收到訊息時的事件
@@ -16,6 +16,21 @@ client.on('message', msg => {
         // console.log(msg.user)
         // 則 Bot 回應 'Pong'
         msg.reply('大大安安');
+    }
+    else if (msg.content === '麒麟哥閉嘴') {
+        // console.log(msg.user)
+        // 則 Bot 回應 'Pong'
+        msg.reply('你這樣讓我很沒面子');
+    }
+    else if (msg.content === '不爽退團阿') {
+        // console.log(msg.user)
+        // 則 Bot 回應 'Pong'
+        msg.reply('老子直接退吞！歐氣氣氣氣氣');
+    }
+    else if (msg.content === '我可以怎麼嗆你') {
+        // console.log(msg.user)
+        // 則 Bot 回應 'Pong'
+        msg.reply('你可以回答 \n 麒麟哥閉嘴 \n 不爽退團阿 \n 這兩個指令來嗆我，如果打安安我會跟你問安歐:D');
     }
 });
 
